62a0bb5021bd2d1a159d654949f04546039ecfb7
mrjohnming <john.ngai@hotmail.com>
2022-04-25 20:12:18 -0400

    Add NavLinks

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,15 +1,39 @@
 import './index.scss';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 
 export default function Sidebar() {
   return (
     <div className='nav-bar'>
       <Link className='logo' to='/'>
         <img src={LogoS} alt='logo' />
-        <img src={LogoSubtitle} alt='slobodan' />
+        <img className='sub-logo ' src={LogoSubtitle} alt='slobodan' />
       </Link>
+
+      <nav>
+        <NavLink to='/' exact='true' activeclassname='active'>
+          <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+        </NavLink>
+
+        <NavLink className='about-link'
+          to='/about'
+          exact='true'
+          activeclassname='active'
+        >
+          <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+        </NavLink>
+
+        <NavLink className='contact-link'
+          to='/contact'
+          exact='true'
+          activeclassname='active'
+        >
+          <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
+        </NavLink>
+      </nav>
     </div>
   );
 }
