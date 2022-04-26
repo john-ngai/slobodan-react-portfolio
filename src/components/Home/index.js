9116983011a38737eacf578fe9c4f3abec871f3b
mrjohnming <john.ngai@hotmail.com>
2022-04-26 14:34:07 -0400

    Replace static letters with animated

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,41 @@
 import './index.scss';
-import LogoTitle from '../../assets/images/logo-s.png';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import LogoTitle from '../../assets/images/logo-s.png';
+import AnimatedLetters from '../AnimatedLetters';
 
 export default function Home() {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n'];
+  const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r'];
+  
   return (
     <div className='container home-page'>
       <div className='text-zone'>
-        <h1>Hi, <br /> I'm
-          <img src={LogoTitle} alt='developer' />
-          lobodan
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          
           <br />
-          web developer
+
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          
+          <img src={LogoTitle} alt='developer' />
+
+          <AnimatedLetters
+            letterClass={letterClass}
+            stringArray={nameArray}
+            index={15}
+          />
+
+          <br />
+          
+          <AnimatedLetters
+            letterClass={letterClass}
+            stringArray={jobArray}
+            index={22}
+          />
         </h1>
         <h2>Frontend Developer / JavaScript Expert / YouTuber</h2>
         <Link className='flat-bottom' to='/contact'>CONTACT ME</Link>
