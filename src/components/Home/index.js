34b85ef9cb411cfa20c28e5842543ff32cae1523
mrjohnming <john.ngai@hotmail.com>
2022-04-26 18:38:35 -0400

    Import <Loader />

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,12 +4,13 @@ import { useEffect, useState } from 'react';
 import LogoTitle from '../../assets/images/logo-s.png';
 import AnimatedLetters from '../AnimatedLetters';
 import Logo from './Logo';
+import Loader from 'react-loaders';
 
 export default function Home() {
   const [letterClass, setLetterClass] = useState('text-animate');
   const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n'];
   const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.'];
-  
+
   useEffect(() => {
     setTimeout(() => {
       setLetterClass('text-animate-hover');
@@ -17,39 +18,42 @@ export default function Home() {
   }, []);
 
   return (
-    <div className='container home-page'>
-      <div className='text-zone'>
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          
-          <br />
+    <>
+      <div className='container home-page'>
+        <div className='text-zone'>
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
 
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          
-          <img
-            src={LogoTitle} alt='JavaScript Developer Name, Web Developer Name'
-          />
+            <br />
 
-          <AnimatedLetters
-            letterClass={letterClass}
-            stringArray={nameArray}
-            index={15}
-          />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
 
-          <br />
-          
-          <AnimatedLetters
-            letterClass={letterClass}
-            stringArray={jobArray}
-            index={22}
-          />
-        </h1>
-        <h2>Frontend Developer / JavaScript Expert / YouTuber</h2>
-        <Link className='flat-bottom' to='/contact'>CONTACT ME</Link>
+            <img
+              src={LogoTitle} alt='JavaScript Developer Name, Web Developer Name'
+            />
+
+            <AnimatedLetters
+              letterClass={letterClass}
+              stringArray={nameArray}
+              index={15}
+            />
+
+            <br />
+
+            <AnimatedLetters
+              letterClass={letterClass}
+              stringArray={jobArray}
+              index={22}
+            />
+          </h1>
+          <h2>Frontend Developer / JavaScript Expert / YouTuber</h2>
+          <Link className='flat-bottom' to='/contact'>CONTACT ME</Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type='pacman' />
+    </>
   );
 }
