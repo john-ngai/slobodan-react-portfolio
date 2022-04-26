301e722d8e66011b9ca8adc7d851f1239c8e31e4
mrjohnming <john.ngai@hotmail.com>
2022-04-26 14:20:34 -0400

    Create component - AnimatedLetters

---
+++ src/components/AnimatedLetters/index.js
@@ -0,0 +1,16 @@
+import './index.scss';
+
+export default function AnimatedLetters({ letterClass, stringArray, index }) {
+  return (
+    <span>
+      {
+        stringArray.map((char, i) => (
+          <span key={char + i} className={`${letterClass} _${i + index}`}>
+            {char}
+          </span>
+        ))
+      }
+    </span>
+  );
+}
+ 
